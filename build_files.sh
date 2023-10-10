dde374a1b5514b8d140b8ba7e8e6cdb7c56886a5
MED-B <77291384+MED-B@users.noreply.github.com>
2023-10-10 15:31:42 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,16 +6,25 @@ echo "Start executing build_files.sh script"
 export MYSQLCLIENT_CFLAGS=""
 export MYSQLCLIENT_LDFLAGS="" 
 
+# Print current working directory
+echo "Current directory: $(pwd)"
+
+# List contents of the current directory
+echo "Directory contents: $(ls)"
+
 # Assuming your Django project is in the AWM directory
 cd AWM
 
+# List contents of the current directory
+echo "Directory contents: $(ls)" 
+
 # Install dependencies
-# pip install -r requirements.txt 
+pip install -r AWM/requirements.txt 
 
 # Install mysqlclient manually
 pip install mysqlclient
 
 # Collect static files
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "End executing build_files.sh script"
