41626608c33e481e3b486a0e216d4932b89989f9
MED-B <77291384+MED-B@users.noreply.github.com>
2023-10-10 15:22:23 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,11 +3,11 @@ echo "Start executing build_files.sh script"
 
 # !/bin/bash
 # Set MySQL client environment variables
-# export MYSQLCLIENT_CFLAGS=""
-# export MYSQLCLIENT_LDFLAGS="" 
+export MYSQLCLIENT_CFLAGS=""
+export MYSQLCLIENT_LDFLAGS="" 
 
 # Assuming your Django project is in the AWM directory
-# cd AWM
+cd AWM
 
 # Install dependencies
 # pip install -r requirements.txt 
